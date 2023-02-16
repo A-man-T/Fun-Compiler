6f2f377f46041acd62cd7b79cdd15516ea41bdce
Aman Tewari <amana@cs.utexas.edu>
2023-02-15 21:47:19 -0600

    report

--- test.fun
+++ test.fun
@@ -1,67 +1,15 @@
-########### testing # many # hashtags # in # a # comment
-
-# make sure we can name the function 'format'
-fun format() {
-# make sure if doesn't return if the statement is false
-    if(0) {
-        return 2
+fun main(){
+    x = 1
+    y = 5
+    z = 3
+    a = 0
+    while(x<10){
+        x=x+1
+        y = 7
+        y = y + 1
+        z = x+z
+        a = x+2
+        
     }
-# make sure while terminates upon return
-    while(1) {
-        return 1
-    }
-}
-
-# basic if, parameters, return test
-fun a(y, z) 
-# comment to test spacing issues
-{
-    x = 5
-    y = x
-    if(z) {
-        return y + x
-    }
-    else 
-# comment to test spacing issues    
-
-
-    {
-        return x
-    }
-}
-
-# testing that you can call functions declared below
-fun b(x) {
-    if(x <= 3) {
-        return 3 * x
-    }
-    return c(x - 1)
-}
-
-fun c(x) {
-#testing that functions can call each other
-    if(x <= 2) {
-        return 2 * x
-    }
-    return b(x - 1)
-}
-
-fun main() {
-    print(format())
-    print(a(1, 2))
-    print(a(2, 0))
-
-#testing if variables can be the same name as functions
-    a = 3
-    format = 5
-
-#testing basic precedence, operators, etc
-    print(format - a % (format / 2))
-    print(format < a >= format - format)
-    print(a(1, 1) - format % a)
-    print(a(1, 1) % (2 * format() + format))
-
-#testing that functions can call each other
-    print(b(5))
-    print(b(6))
+    print(x)
 }
--- test.s
+++ test.s
@@ -29,214 +29,11 @@ main:
       pop %rbx
       pop %rbp
       retq
-._format:
-      push %rbp
-      mov %rsp,%rbp
-      add $-8,%rsp 
-      mov $0,%r13
-      push %r13
-      pop %rdi
-      test %rdi,%rdi
-        jz .0
-      mov $2,%r13
-      push %r13
-      pop %rax
-      mov %rbp, %rsp
-      pop %rbp
-      retq
-        jmp .1
-.0:
-.1:
-.2:
-      mov $1,%r13
-      push %r13
-      pop %rdi
-      test %rdi,%rdi
-        jz .3
-      mov $1,%r13
-      push %r13
-      pop %rax
-      mov %rbp, %rsp
-      pop %rbp
-      retq
-        jmp .2
-.3:
-      mov $0,%rax
-      sub $-8,%rsp 
-      pop %rbp
-      retq
-._a:
-      push %rbp
-      mov %rsp,%rbp
-      add $-8,%rsp 
-      mov $5,%r13
-      push %r13
-      pop %rdi
-      mov %rdi,-8(%rbp)
-      mov -8(%rbp),%rdi
-      push %rdi
-      pop %rdi
-      mov %rdi,24(%rbp)
-      mov 16(%rbp),%rdi
-      push %rdi
-      pop %rdi
-      test %rdi,%rdi
-        jz .4
-      mov 24(%rbp),%rdi
-      push %rdi
-      mov -8(%rbp),%rdi
-      push %rdi
-      pop %rdx
-      pop %rax
-      add %rdx, %rax
-      push %rax
-      pop %rax
-      mov %rbp, %rsp
-      pop %rbp
-      retq
-        jmp .5
-.4:
-      mov -8(%rbp),%rdi
-      push %rdi
-      pop %rax
-      mov %rbp, %rsp
-      pop %rbp
-      retq
-.5:
-      mov $0,%rax
-      sub $-8,%rsp 
-      pop %rbp
-      retq
-._b:
-      push %rbp
-      mov %rsp,%rbp
-      add $0,%rsp 
-      mov 16(%rbp),%rdi
-      push %rdi
-      mov $3,%r13
-      push %r13
-      pop %rdx
-      pop %rax
-      cmp %rdx, %rax
-      mov $0,%rax
-      setbe %al
-      push %rax
-      pop %rdi
-      test %rdi,%rdi
-        jz .6
-      mov $3,%r13
-      push %r13
-      mov 16(%rbp),%rdi
-      push %rdi
-      pop %rdx
-      pop %rax
-      imul %rdx, %rax
-      push %rax
-      pop %rax
-      mov %rbp, %rsp
-      pop %rbp
-      retq
-        jmp .7
-.6:
-.7:
-      mov 16(%rbp),%rdi
-      push %rdi
-      mov $1,%r13
-      push %r13
-      pop %rdx
-      pop %rax
-      sub %rdx, %rax
-      push %rax
-      call ._c
-      add $8,%rsp
-      push %rax
-      pop %rax
-      mov %rbp, %rsp
-      pop %rbp
-      retq
-      mov $0,%rax
-      sub $0,%rsp 
-      pop %rbp
-      retq
-._c:
-      push %rbp
-      mov %rsp,%rbp
-      add $0,%rsp 
-      mov 16(%rbp),%rdi
-      push %rdi
-      mov $2,%r13
-      push %r13
-      pop %rdx
-      pop %rax
-      cmp %rdx, %rax
-      mov $0,%rax
-      setbe %al
-      push %rax
-      pop %rdi
-      test %rdi,%rdi
-        jz .8
-      mov $2,%r13
-      push %r13
-      mov 16(%rbp),%rdi
-      push %rdi
-      pop %rdx
-      pop %rax
-      imul %rdx, %rax
-      push %rax
-      pop %rax
-      mov %rbp, %rsp
-      pop %rbp
-      retq
-        jmp .9
-.8:
-.9:
-      mov 16(%rbp),%rdi
-      push %rdi
-      mov $1,%r13
-      push %r13
-      pop %rdx
-      pop %rax
-      sub %rdx, %rax
-      push %rax
-      call ._b
-      add $8,%rsp
-      push %rax
-      pop %rax
-      mov %rbp, %rsp
-      pop %rbp
-      retq
-      mov $0,%rax
-      sub $0,%rsp 
-      pop %rbp
-      retq
 ._main:
       push %rbp
       mov %rsp,%rbp
-      add $-16,%rsp 
-      call ._format
-      add $0,%rsp
-      push %rax
-      call ._print
-      pop %r13
+      add $-32,%rsp 
       mov $1,%r13
-      push %r13
-      mov $2,%r13
-      push %r13
-      call ._a
-      add $16,%rsp
-      push %rax
-      call ._print
-      pop %r13
-      mov $2,%r13
-      push %r13
-      mov $0,%r13
-      push %r13
-      call ._a
-      add $16,%rsp
-      push %rax
-      call ._print
-      pop %r13
-      mov $3,%r13
       push %r13
       pop %rdi
       mov %rdi,-8(%rbp)
@@ -244,122 +41,79 @@ main:
       push %r13
       pop %rdi
       mov %rdi,-16(%rbp)
-      mov -16(%rbp),%rdi
-      push %rdi
-      mov -8(%rbp),%rdi
-      push %rdi
-      mov -16(%rbp),%rdi
-      push %rdi
-      mov $2,%r13
+      mov $3,%r13
       push %r13
-      xor %rdx,%rdx
       pop %rdi
-      pop %rax
-      divq %rdi
-      push %rax
-      xor %rdx,%rdx
+      mov %rdi,-24(%rbp)
+      mov $0,%r13
+      push %r13
       pop %rdi
-      pop %rax
-      divq %rdi
-      push %rdx
-      pop %rdx
-      pop %rax
-      sub %rdx, %rax
-      push %rax
-      call ._print
-      pop %r13
-      mov -16(%rbp),%rdi
-      push %rdi
+      mov %rdi,-32(%rbp)
+.0:
       mov -8(%rbp),%rdi
       push %rdi
+      mov $10,%r13
+      push %r13
       pop %rdx
       pop %rax
       cmp %rdx, %rax
       mov $0,%rax
       setb %al
       push %rax
-      mov -16(%rbp),%rdi
-      push %rdi
-      mov -16(%rbp),%rdi
-      push %rdi
-      pop %rdx
-      pop %rax
-      sub %rdx, %rax
-      push %rax
-      pop %rdx
-      pop %rax
-      cmp %rdx, %rax
-      mov $0,%rax
-      setae %al
-      push %rax
-      call ._print
-      pop %r13
-      mov $1,%r13
-      push %r13
-      mov $1,%r13
-      push %r13
-      call ._a
-      add $16,%rsp
-      push %rax
-      mov -16(%rbp),%rdi
-      push %rdi
+      pop %rdi
+      test %rdi,%rdi
+        jz .1
       mov -8(%rbp),%rdi
       push %rdi
-      xor %rdx,%rdx
+      mov $1,%r13
+      push %r13
+      pop %rdx
+      pop %rax
+      add %rdx, %rax
+      push %rax
       pop %rdi
-      pop %rax
-      divq %rdi
-      push %rdx
-      pop %rdx
-      pop %rax
-      sub %rdx, %rax
-      push %rax
-      call ._print
-      pop %r13
-      mov $1,%r13
+      mov %rdi,-8(%rbp)
+      mov $7,%r13
       push %r13
-      mov $1,%r13
-      push %r13
-      call ._a
-      add $16,%rsp
-      push %rax
-      mov $2,%r13
-      push %r13
-      call ._format
-      add $0,%rsp
-      push %rax
-      pop %rdx
-      pop %rax
-      imul %rdx, %rax
-      push %rax
+      pop %rdi
+      mov %rdi,-16(%rbp)
       mov -16(%rbp),%rdi
+      push %rdi
+      mov $1,%r13
+      push %r13
+      pop %rdx
+      pop %rax
+      add %rdx, %rax
+      push %rax
+      pop %rdi
+      mov %rdi,-16(%rbp)
+      mov -8(%rbp),%rdi
+      push %rdi
+      mov -24(%rbp),%rdi
       push %rdi
       pop %rdx
       pop %rax
       add %rdx, %rax
       push %rax
-      xor %rdx,%rdx
       pop %rdi
+      mov %rdi,-24(%rbp)
+      mov -8(%rbp),%rdi
+      push %rdi
+      mov $2,%r13
+      push %r13
+      pop %rdx
       pop %rax
-      divq %rdi
-      push %rdx
-      call ._print
-      pop %r13
-      mov $5,%r13
-      push %r13
-      call ._b
-      add $8,%rsp
+      add %rdx, %rax
       push %rax
-      call ._print
-      pop %r13
-      mov $6,%r13
-      push %r13
-      call ._b
-      add $8,%rsp
-      push %rax
+      pop %rdi
+      mov %rdi,-32(%rbp)
+        jmp .0
+.1:
+      mov -8(%rbp),%rdi
+      push %rdi
       call ._print
       pop %r13
       mov $0,%rax
-      sub $-16,%rsp 
+      sub $-32,%rsp 
       pop %rbp
       retq
